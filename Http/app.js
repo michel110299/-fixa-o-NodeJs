34f2319db3ea0d822dc22fdfb49560a2236e3b65
Michel Fernandes Lemes <michel.lemes@unincor.edu.br>
2021-09-16 18:07:05 -0300

    comentários implementado

--- Http/app.js
+++ Http/app.js
@@ -1,8 +1,7 @@
 const { read } = require("fs");
-var http = require("http")
+var http = require("http") //import modulo http
 
 http.createServer(function(req,res){
-    res.end("Hello Word!")
-}).listen(8081);
-
+    res.end("Hello Word!")//conteudo
+}).listen(8081); //porta do servidor
 console.log("o servidor rodando! ")
